82b8830c140c5c66de99ea1faa30bfad0c9cc750
sepehr-amooei <sepehramooei@gmail.com>
2023-08-01 10:56:44 +0330

    disabling submit button

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -1,89 +1,93 @@
-import React, { Component } from 'react';
-import  Joi  from 'joi-browser';
-import Input from './input';
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./input";
 
 class LoginForm extends Component {
+  //username = React.createRef();this will creat a ref object.you should minimize your use of refs,for example manage the foucs of an input field
+  // componentDidMount() {
+  //  this.username.current.focus();
+  // }
 
- //username = React.createRef();this will creat a ref object.you should minimize your use of refs,for example manage the foucs of an input field
- // componentDidMount() {
- //  this.username.current.focus();
- // }
-
- state = {
-  account: { username: '', password: '' },
-  errors: {}
- };
+  state = {
+    account: { username: "", password: "" },
+    errors: {},
+  };
 
   schema = {
     username: Joi.string().required().label("Username"),
-    password: Joi.string().required().label("Password")
-  }
+    password: Joi.string().required().label("Password"),
+  };
 
-  validate = () => {//validate entire form
+  validate = () => {
+    //validate entire form
 
-    const {error} = Joi.validate(this.state.account, this.schema, { abortEarly: false });
-    
+    const { error } = Joi.validate(this.state.account, this.schema, {
+      abortEarly: false,
+    });
+
     if (!error) return null;
     const errors = {};
-      for (let item of error.details) {
+    for (let item of error.details) {
       errors[item.path[0]] = item.message;
     }
-  
-    return errors;
-}
 
- handleSubmit = e => {
-  e.preventDefault();
-  const errors = this.validate();
-  this.setState({errors : errors || {}});
-  if (errors) return;
-  //Call the sever
-  console.log("Submitted");
-  }
-  validateProperty = ({name, value}) => {//validate on input
+    return errors;
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+    //Call the sever
+    console.log("Submitted");
+  };
+  validateProperty = ({ name, value }) => {
+    //validate on input
     const obj = { [name]: value };
-    const schema = { [name]: this.schema[name] }
-    const { error } = Joi.validate(obj, schema);// we want to abort early, beacuse there might be more than one validation errors with the given field, we dont want to display all those validation errors at once, beacuse form a usibility point of view that is confusing. 
+    const schema = { [name]: this.schema[name] };
+    const { error } = Joi.validate(obj, schema); // we want to abort early, beacuse there might be more than one validation errors with the given field, we dont want to display all those validation errors at once, beacuse form a usibility point of view that is confusing.
     return error ? error.details[0].message : null;
-  }
+  };
 
   handleChange = ({ currentTarget: input }) => {
-  const errors = { ...this.state.errors };
-  const errorMassage = this.validateProperty(input);
-  if (errorMassage) errors[input.name] = errorMassage;
-  else delete errors[input.name];
+    const errors = { ...this.state.errors };
+    const errorMassage = this.validateProperty(input);
+    if (errorMassage) errors[input.name] = errorMassage;
+    else delete errors[input.name];
 
-  const account = { ...this.state.account };
-  account[input.name] = input.value;
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
     this.setState({ account, errors });
-    
- }
+  };
 
- render() { 
-  const { account, errors } = this.state; 
-  return (
-   <div>
-    <h1>Login</h1>
-    <form onSubmit={this.handleSubmit}>
-     <Input
-      name='username'
-      label='Username'
-      value={account.username}
-      onChange={this.handleChange}
-      error = {errors.username}
-     />
-     <Input
-      name='password'
-      label='Password'
-      value={account.password}
-      onChange={this.handleChange}
-      error = {errors.password}
-     />
-     <button className="btn btn-primary">Login</button>
-    </form>
-   </div>
-  );
- }
+  render() {
+    const { account, errors } = this.state;
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          <Input
+            name="username"
+            label="Username"
+            value={account.username}
+            onChange={this.handleChange}
+            error={errors.username}
+          />
+          <Input
+            name="password"
+            label="Password"
+            value={account.password}
+            onChange={this.handleChange}
+            error={errors.password}
+          />
+          <button className="btn btn-primary" disabled={this.validate()}>
+            Login
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default LoginForm;
